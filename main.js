fa63a979b800ae24543ff9b6011a3cad603f9998
Hrishikesh2012 <133791262+Hrishikesh2012@users.noreply.github.com>
2023-05-17 15:30:56 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,34 @@
+var canvas = new fabric.Canvas('myCanvas');
+ball_y=0;
+ball_x=0;
+hole_y=400;
+hole_x=800;
+function load_img()
+{
+fabric.Image.fromURL("golf-hi.png" , function (Img) {
+  hole_obj = Img;
+  hole_obj.scaletoWidth(50);
+  hole_obj.scaletoHeigth(50);
+  hole_obj.set({
+    top:hole_y,
+    left:hole_x   
+  });
+canvas.add(hole_obj);
+  });
+  new_image();
+}
+
+function new_image()
+{
+fabric.Image.fromURL("ball.png" , function (Img) {
+  ball_obj = Img;
+  ball_obj.scaletoWidth(50);
+  ball_obj.scaletoHeigth(50);
+  ball_obj.set({
+    top:ball_y,
+    left:ball_x   
+  });
+canvas.add(ball_obj);
+  });
+  new_image();
+}
